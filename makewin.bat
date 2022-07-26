c090f4bb72485d5fd199cb09343c4e3674d9f4fd
Никита <50713261+NikitaDmitryuk@users.noreply.github.com>
2022-07-26 23:15:20 +0300

    Fix makewin

--- makewin.bat
+++ makewin.bat
@@ -1,4 +1,4 @@
 @echo off
 SETLOCAL
 
-docker run --rm -i -v %cd%:/diplom:Z 2109199812/docker-latex bash -c "make release"
+docker run --rm -i -v "%cd%":/diplom:Z 2109199812/docker-latex bash -c "make release"
