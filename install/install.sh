9446462434dc76986aac619926f018cbbf1d69ee
NikitaDmitryukExponenta <Nikita.Dmitryuk@exponenta.ru>
2022-07-27 18:21:36 +0300

    Fix install

--- install/install.sh
+++ install/install.sh
@@ -3,12 +3,15 @@
 sudo pacman -S docker
 sudo systemctl --now enable docker
 sudo setfacl -m user:${USER}:rw /var/run/docker.sock
+sudo usermod -aG docker ${USER}
 sudo systemctl restart docker
 
 if [[ "$1" == "build" ]]; then
     sudo docker build -t 2109199812/docker-latex .
-	exit
 else
     docker pull 2109199812/docker-latex
-    exit
 fi
+
+echo "Restart PC"
+
+exit
