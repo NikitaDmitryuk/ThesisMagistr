e3f34ca21d4fb0110836acd08ea5b9602da8435a
NikitaDmitryuk <dmitryuk.nikita@gmail.com>
2023-03-16 23:12:46 +0300

    Fix docker build image

--- install/install.sh
+++ install/install.sh
@@ -25,7 +25,7 @@ configure_docker() {
 pull_or_build_docker_image() {
     if [ "$1" == "build" ]; then
         echo "Сборка Docker-образа для LaTeX..."
-        docker build -t 2109199812/docker-latex .
+        docker build --load -t 2109199812/docker-latex .
     else
         echo "Загрузка Docker-образа для LaTeX..."
         docker pull 2109199812/docker-latex
