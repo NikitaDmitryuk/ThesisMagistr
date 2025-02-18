99cd9192e4aa3758c49bb22cbd99b18682dbd146
NikitaDmitryuk <dmitryuk.nikita@gmail.com>
2025-02-18 21:29:47 +0300

    Switch to ubuntu 22.04 and small fixes

--- install/install.sh
+++ install/install.sh
@@ -1,27 +1,5 @@
 #!/bin/sh
 
-install_docker() {
-    if ! command -v docker >/dev/null 2>&1; then
-        if command -v pacman >/dev/null 2>&1; then
-            echo "Установка Docker..."
-            sudo pacman -S docker
-        else
-            echo "Не найден пакетный менеджер pacman. Установите Docker вручную."
-            exit 1
-        fi
-    else
-        echo "Docker уже установлен."
-    fi
-}
-
-configure_docker() {
-    echo "Настройка Docker..."
-    sudo systemctl --now enable docker
-    sudo setfacl -m user:"${USER}":rw /var/run/docker.sock
-    sudo usermod -aG docker "${USER}"
-    sudo systemctl restart docker
-}
-
 pull_or_build_docker_image() {
     if [ "$1" == "build" ]; then
         echo "Сборка Docker-образа для LaTeX..."
@@ -33,10 +11,7 @@ pull_or_build_docker_image() {
 }
 
 main() {
-    install_docker
-    configure_docker
     pull_or_build_docker_image "$1"
-
 }
 
 main "$@"
