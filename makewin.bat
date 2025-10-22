b54e7c54ea83caeacbca126020decc772527f51b
NikitaDmitryuk <dmitryuk.nikita@gmail.com>
2025-10-22 22:03:01 +0300

    Fix makewin.bat

--- makewin.bat
+++ makewin.bat
@@ -4,7 +4,7 @@ SETLOCAL
 rem - Used for commenting
 
 rem Build all .tex files in the main directory
-docker run --rm -i -v "%cd%":/diplom:Z 2109199812/docker-latex bash -c "make release"
+docker run --rm -i -v "%cd%":/diplom:Z ghcr.io/nikitadmitryuk/thesismagistr/latex:latest bash -c "make release"
 
 rem Build diploma.pdf file
 rem docker run --rm -i -v "%cd%":/diplom:Z 2109199812/docker-latex bash -c "make diploma"
